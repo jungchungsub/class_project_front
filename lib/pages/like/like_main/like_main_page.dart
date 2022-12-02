fdc2de1135ef04ca2ef09ef1b59210ceff7305dd
jungchungsub <cndtjq1248@naver.com>
2022-12-02 19:16:06 +0900

    찜목록폴더정리

--- lib/pages/like/like_main/like_main_page.dart
+++ lib/pages/like/like_main/like_main_page.dart
@@ -1,4 +1,3 @@
-import 'package:finalproject_front/pages/like/like_main/components/like_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,19 +15,103 @@ class _LikeMainPageState extends State<LikeMainPage> {
       appBar: _buildAppbar(),
       body: ListView(
         children: [
-          LikeList(
-              routePath: "/lessonDetail", image: "https://picsum.photos/201"),
-          LikeList(
-              routePath: "/lessonDetail", image: "https://picsum.photos/202"),
-          LikeList(
-              routePath: "/lessonDetail", image: "https://picsum.photos/203"),
-          LikeList(
-              routePath: "/lessonDetail", image: "https://picsum.photos/204"),
-          LikeList(
-              routePath: "/lessonDetail", image: "https://picsum.photos/205"),
-          LikeList(
-              routePath: "/lessonDetail", image: "https://picsum.photos/206")
+          _buildLikeList("/lessonDetail", "https://picsum.photos/201"),
+          _buildLikeList("/lessonDetail", "https://picsum.photos/202"),
+          _buildLikeList("/lessonDetail", "https://picsum.photos/203"),
+          _buildLikeList("/lessonDetail", "https://picsum.photos/204"),
+          _buildLikeList("/lessonDetail", "https://picsum.photos/205"),
+          _buildLikeList("/lessonDetail", "https://picsum.photos/206"),
         ],
+      ),
+    );
+  }
+
+  Padding _buildLikeList(String routePath, String image) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16, right: 10, bottom: 8, left: 10),
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, "${routePath}");
+        },
+        child: Container(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8, left: 8, top: 4),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    height: 90,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          image: NetworkImage("${image}"), fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 220,
+                        height: 50,
+                        child: Text(
+                          "깔끔하고 아름다운 웹디자인을 해드립니다아아.",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            CupertinoIcons.star_fill,
+                            color: Colors.yellow,
+                            size: 16,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            "|   45개의 평가",
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            "50,000원",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 140,
+                          ),
+                          Icon(
+                            CupertinoIcons.heart_fill,
+                            color: Colors.red,
+                            size: 20,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
