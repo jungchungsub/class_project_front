18a5ab8e4dbec552aae055238c0b291aa20eae23
jungchungsub <cndtjq1248@naver.com>
2022-12-02 17:37:52 +0900

    리뷰작성 폴더 수정

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ import 'package:finalproject_front/pages/lesson/lesson_detail_page.dart';
 import 'package:finalproject_front/pages/chat/chat_list_page.dart';
 
 import 'package:finalproject_front/pages/lesson/lesson_detail_page.dart';
-import 'package:finalproject_front/pages/lesson/lesson_list/lesson_list_page.dart';
-import 'package:finalproject_front/pages/lesson/lesson_review/lesson_review_insert_page.dart';
+import 'package:finalproject_front/pages/lesson/lesson_list_page.dart';
+import 'package:finalproject_front/pages/lesson/lesson_review_insert_page.dart';
 import 'package:finalproject_front/pages/lesson/lesson_update/lesson_update_page.dart';
 
 import 'package:finalproject_front/pages/main_page.dart';
