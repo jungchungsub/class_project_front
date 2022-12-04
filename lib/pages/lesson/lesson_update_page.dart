69aa585924fa4338168b820710609d9d4b4d2c35
jungchungsub <33271607+jungchungsub@users.noreply.github.com>
2022-12-04 16:49:35 +0900

    [정충섭] Lesson file topic 컨벤션 수정 (#44)
    * lesson_detail파일수정완료
    
    * 리뷰작성 폴더 수정
    
    * 클래스수정 파일정리 완료
    
    * 오류수정
    
    * 잠깐저장
    
    * 수정해결
    
    * 잠깐저장
    
    * 잠깐저장
    
    * 파일정리 완료
    
    * 컨벤션 변경

--- lib/pages/lesson/lesson_update_page.dart
+++ lib/pages/lesson/lesson_update_page.dart
@@ -43,11 +43,11 @@ class _LessonUpdatePageState extends State<LessonUpdatePage> {
             child: Column(
               children: [
                 _buildLessonImage(),
-                ContentBox(scrollAnimate, text: "서비스제목", content: "서비스 제목자리입니다", lines: 1),
-                ContentBox(scrollAnimate, text: "커리큘럼", content: "상세설명", lines: 6),
-                ContentBox(scrollAnimate, text: "수강횟수", content: "수강 횟수를 입력하세요", lines: 1),
-                ContentBox(scrollAnimate, text: "수강시간", content: "수강 시간을 입력하세요", lines: 1),
-                ContentBox(scrollAnimate, text: "수강장소", content: "ex) 부산시 진구 그린아카데미", lines: 1),
+                ContentBox(scrollAnimate, text: "서비스제목", hintText: "서비스 제목자리입니다", lines: 1),
+                ContentBox(scrollAnimate, text: "커리큘럼", hintText: "상세설명", lines: 6),
+                ContentBox(scrollAnimate, text: "수강횟수", hintText: "수강 횟수를 입력하세요", lines: 1),
+                ContentBox(scrollAnimate, text: "수강시간", hintText: "수강 시간을 입력하세요", lines: 1),
+                ContentBox(scrollAnimate, text: "수강장소", hintText: "ex) 부산시 진구 그린아카데미", lines: 1),
                 _buildLessonCategory(),
                 _buildLessonDeadLine(),
                 _buildLessonBottomButton(),
