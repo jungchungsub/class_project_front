2f4347aaf809f2db9c5801eba23f89a3fa338807
seong9566 <dlgustjd9566@gmail.com>
2022-12-07 10:10:23 +0900

    lessonRespDto작성, ReviewRespDto작성

---
+++ lib/dto/response/lesson_resp_dto.dart
@@ -0,0 +1,59 @@
+import 'dart:ffi';
+
+import 'review_resp_dto.dart';
+
+class LessonRespDto {
+  String lessonName;
+  Int lessonPrice;
+  Int lessonTime;
+  Int lessonCount;
+  String lessonPlace;
+  String possibleDays;
+  String lessonPolicy;
+  String masterName;
+  String masterImg;
+  String masterIntroduction;
+  List<ReviewRespDto> lessonReviewList;
+
+  LessonRespDto(
+      {required this.lessonName,
+      required this.lessonPrice,
+      required this.lessonTime,
+      required this.lessonCount,
+      required this.lessonPlace,
+      required this.possibleDays,
+      required this.lessonPolicy,
+      required this.masterName,
+      required this.masterImg,
+      required this.masterIntroduction,
+      required this.lessonReviewList});
+
+  factory LessonRespDto.fromJson(Map<String, dynamic> json) => LessonRespDto(
+        lessonName: json["lessonName"],
+        lessonPrice: json["lessonPrice"],
+        lessonTime: json["lessonTime"],
+        lessonCount: json["lessonCount"],
+        lessonPlace: json["lessonPlace"],
+        possibleDays: json["possibleDays"],
+        lessonPolicy: json["lessonPolicy"],
+        masterName: json["masterName"],
+        masterImg: json["masterImg"],
+        masterIntroduction: json["masterIntroduction"],
+        //Nested Structure이기 때문에 한번에 map처리 null이 아니면 fromjson을 실행.
+        lessonReviewList: (json["lessonReviewList"]).map((e) => e == null ? null : ReviewRespDto.fromJson(e).toJson()),
+      );
+  Map<String, dynamic> toJson() => {
+        "lessonName": lessonName,
+        "lessonPrice": lessonPrice,
+        "lessonTime": lessonTime,
+        "lessonCount": lessonCount,
+        "lessonPlace": lessonPlace,
+        "possibleDays": possibleDays,
+        "lessonPolicy": lessonPolicy,
+        "masterName": masterName,
+        "masterImg": masterImg,
+        "masterIntroduction": masterIntroduction,
+        //Nested Structure이기 때문에 한번에 map처리
+        "lessonReviewList": lessonReviewList.map((e) => e.toJson()).toList(),
+      };
+}
---
+++ lib/dto/response/review_resp_dto.dart
@@ -0,0 +1,27 @@
+import 'dart:ffi';
+
+class ReviewRespDto {
+  // 테스트를 위해서 임의로 작성, 이후에 지워야함.
+
+  String username;
+  String reviewContent;
+  Double lessonGrade;
+
+  ReviewRespDto({
+    required this.username,
+    required this.reviewContent,
+    required this.lessonGrade,
+  });
+
+  factory ReviewRespDto.fromJson(Map<String, dynamic> json) => ReviewRespDto(
+        username: json["username"],
+        reviewContent: json["reviewContent"],
+        lessonGrade: json["lessonGrade"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        "reviewContent": reviewContent,
+        "lessonGrade": lessonGrade,
+      };
+}
