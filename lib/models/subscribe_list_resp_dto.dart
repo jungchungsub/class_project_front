003b897f926907cba8468242424789f6baede972
jungchungsub <cndtjq1248@naver.com>
2022-12-08 10:26:12 +0900

    좋아요페이지더미 바인딩 완료

--- lib/models/subscribe_list_resp_dto.dart
+++ lib/models/subscribe_list_resp_dto.dart
@@ -40,7 +40,7 @@ List<SubscribeListRespDto> subscribeListRespDto = [
         id: 1,
         name: "깔끔하고 아름다운 웹 디자인을 해드립니다.",
         photo: "https://picsum.photos/201",
-        price: 50000,
+        price: 40000,
       )),
   SubscribeListRespDto(
       id: 2,
@@ -48,8 +48,8 @@ List<SubscribeListRespDto> subscribeListRespDto = [
       lessonDto: LessonDto(
         id: 1,
         name: "깔끔하고 아름다운 웹 디자인을 해드립니다.",
-        photo: "https://picsum.photos/201",
-        price: 50000,
+        photo: "https://picsum.photos/202",
+        price: 30000,
       )),
   SubscribeListRespDto(
       id: 3,
@@ -57,7 +57,34 @@ List<SubscribeListRespDto> subscribeListRespDto = [
       lessonDto: LessonDto(
         id: 1,
         name: "깔끔하고 아름다운 웹 디자인을 해드립니다.",
-        photo: "https://picsum.photos/201",
+        photo: "https://picsum.photos/203",
+        price: 50000,
+      )),
+  SubscribeListRespDto(
+      id: 4,
+      userDto: UserDto(username: "아이유"),
+      lessonDto: LessonDto(
+        id: 1,
+        name: "깔끔하고 아름다운 웹 디자인을 해드립니다.",
+        photo: "https://picsum.photos/204",
+        price: 50000,
+      )),
+  SubscribeListRespDto(
+      id: 5,
+      userDto: UserDto(username: "아이유"),
+      lessonDto: LessonDto(
+        id: 1,
+        name: "아름다운 웹 디자인을 해드립니다.",
+        photo: "https://picsum.photos/205",
+        price: 50000,
+      )),
+  SubscribeListRespDto(
+      id: 6,
+      userDto: UserDto(username: "아이유"),
+      lessonDto: LessonDto(
+        id: 1,
+        name: "깔끔하고 아름다운 웹 디자인을 해드립니다.",
+        photo: "https://picsum.photos/206",
         price: 50000,
       ))
 ];
--- lib/pages/subscribe/subscribe_main_page.dart
+++ lib/pages/subscribe/subscribe_main_page.dart
@@ -18,102 +18,8 @@ class _SubscribeMainPageState extends State<SubscribeMainPage> {
             shrinkWrap: true, //리스트 자식 높이 크기의 합 만큼으로 영역을 고정 시켜준다.
             itemCount: subscribeListRespDto.length,
             itemBuilder: ((BuildContext context, int index) {
-              return _buildLikeList(
-                "/lessonDetail",
-                "https://picsum.photos/201",
-                "깔끔하고 아름다운 웹디자인을 해드립니다.",
-                45,
-                "50,000",
-              );
+              return SubscribeLesson(itemIndex: index);
             })));
-  }
-
-  Padding _buildLikeList(String routePath, String imagePath, String likeTitle, int likeEvaluation, String price) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 16, right: 10, bottom: 8, left: 10),
-      child: InkWell(
-        onTap: () {
-          Navigator.pushNamed(context, "${routePath}");
-        },
-        child: Container(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 8, left: 8, top: 4),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(
-                    height: 90,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(image: NetworkImage("${imagePath}"), fit: BoxFit.cover),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 220,
-                        height: 50,
-                        child: Text(
-                          "${likeTitle}",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            CupertinoIcons.star_fill,
-                            color: Colors.yellow,
-                            size: 16,
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text(
-                            "|  ${likeEvaluation}개의 평가",
-                            style: TextStyle(fontSize: 14),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text(
-                            "${price}원",
-                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(
-                            width: 140,
-                          ),
-                          Icon(
-                            CupertinoIcons.heart_fill,
-                            color: Colors.red,
-                            size: 20,
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
   }
 
   AppBar _buildAppbar() {
@@ -142,6 +48,100 @@ class _SubscribeMainPageState extends State<SubscribeMainPage> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class SubscribeLesson extends StatelessWidget {
+  final int itemIndex;
+  const SubscribeLesson({required this.itemIndex, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16, right: 10, bottom: 8, left: 10),
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, "/lessonDetail");
+        },
+        child: Container(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8, left: 8, top: 4),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    height: 90,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(image: NetworkImage("${subscribeListRespDto[itemIndex].lessonDto.photo}"), fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 220,
+                        height: 50,
+                        child: Text(
+                          "${subscribeListRespDto[itemIndex].lessonDto.name}",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            CupertinoIcons.star_fill,
+                            color: Colors.yellow,
+                            size: 16,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            "|  55개의 평가",
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            "${subscribeListRespDto[itemIndex].lessonDto.price}",
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 140,
+                          ),
+                          Icon(
+                            CupertinoIcons.heart_fill,
+                            color: Colors.red,
+                            size: 20,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
