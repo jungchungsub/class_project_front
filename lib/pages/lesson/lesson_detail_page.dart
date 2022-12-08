bd4bf89fa7741587de183b22158e9cd59d0ad067
jungchungsub <cndtjq1248@naver.com>
2022-12-08 15:52:29 +0900

    lesson_detail수정

--- lib/pages/lesson/lesson_detail_page.dart
+++ lib/pages/lesson/lesson_detail_page.dart
@@ -48,7 +48,7 @@ class LessonDetailPage extends ConsumerWidget {
                             Padding(
                               padding: const EdgeInsets.only(top: 16, bottom: 16),
                               child: Text(
-                                "120,000원",
+                                "${lessonList[0].lessonPrice}원",
                                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                               ),
                             ),
@@ -358,81 +358,6 @@ class LessonDetailPage extends ConsumerWidget {
                     fontSize: 16,
                   ),
                 )),
-          ),
-          SizedBox(
-            height: 20,
-          )
-        ],
-      ),
-    );
-  }
-
-  Container _buildLessonTime() {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "수강시간",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Container(
-            height: 100,
-            decoration: BoxDecoration(
-              color: const Color(0xffEAF2FD),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "1회당 레슨시간(분)",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(
-                        "105분",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "레슨 횟수",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(
-                        "10회",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
           ),
           SizedBox(
             height: 20,
