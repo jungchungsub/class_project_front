9052457cf132a6a043cdd06edbe3d62a02fb7839
jungchungsub <cndtjq1248@naver.com>
2022-12-14 20:43:43 +0900

    레슨추가하기 하다가 집에감

--- lib/service/lesson_service.dart
+++ lib/service/lesson_service.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:finalproject_front/core/http_connector.dart';
+import 'package:finalproject_front/dto/request/lesson_req_dto.dart';
 import 'package:finalproject_front/dto/response/lesson_latest_list_resp_dto.dart';
 import 'package:finalproject_front/dto/response/lesson_resp_dto.dart';
 import 'package:finalproject_front/dto/response/respone_dto.dart';
@@ -13,22 +14,16 @@ class LessonService {
 
   Future<ResponseDto> getLessonDetail(int lessonId, String? jwtToken) async {
     //Logger().d("id출력service:${lessonId}");
-
     Response response = await httpConnector.get(path: "/api/category/lesson/${lessonId}", jwtToken: jwtToken);
-
     ResponseDto responseDto = toResponseDto(response);
-
     Logger().d(response.body);
-
     final value = responseDto.data["lessonAvgGrade"];
     Logger().d("value 값 : ${value}");
     if (value == "NaN") {
       responseDto.data["lessonAvgGrade"] = 0.0;
     }
-
     Logger().d(responseDto.data["lessonAvgGrade"]);
     Logger().d("~~~~~~~~~~~~~");
-
     responseDto.data = LessonRespDto.fromJson(responseDto.data);
     //Logger().d("데이터확인 : ${responseDto.data}");
     return responseDto;
@@ -50,13 +45,13 @@ class LessonService {
     return responseDto;
   }
 
-  // Future<ResponseDto> categoryList(String? jwtToken) async {
-  //   Response response  = await httpConnector.get(path: "/api/category/${categoryId}",jwtToken: jwtToken);
+  // Future<ResponseDto> lessonInsert(LessonReqDto lessonReqDto) async {
+  //   Response response = await httpConnector.post("/api/lesson", lessonReqDto);
+  //   String jwtToken = response.headers["authorization"].toString();
+
   //   ResponseDto responseDto = toResponseDto(response);
-  //   if(responseDto.statusCode< 300){
-  //           List<dynamic> mapList = responseDto.data;
-  //           List<>
+  //   if (responseDto.statusCode < 300) {
+  //     // responseDto.data =
   //   }
   // }
-
 }
